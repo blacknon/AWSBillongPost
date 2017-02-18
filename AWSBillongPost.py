232085ef70e59fc035e96528a6980abce2e206f7
uesugi <uesugi@uesugi-no-MacBook-Pro.local>
2017-02-18 23:25:37 +0900

    add dirrerence display

--- AWSBillongPost.py
+++ AWSBillongPost.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
 import os,sys,json,datetime
 import ConfigParser
 import boto3
 import slackweb
 
 inifile = ConfigParser.SafeConfigParser()
-inifile.read('/opt/script/.auth_info')
+inifile.read('/home/sprout/.auth_info')
 
 # ==== AWS認証情報 ====
 REGION = "us-east-1"
@@ -26,19 +25,20 @@ cloudwatch_conn = aws_session.client('cloudwatch')
 
 # ==== Slack認証設定 ====
 SLACK_WEBHOOK=inifile.get('slack', 'WEBHOOK_URL')
-SLACK_CHANNEL='#intranet'
-SLACK_USER='AWSBillingInfo'
+SLACK_CHANNEL=inifile.get('slack', 'CHANNEL')
+SLACK_USER=inifile.get('slack', 'USER')
 
 
 # ==== 昨日/一昨日の日付を取得する ====
-last_date = (datetime.date.today() -datetime.timedelta(1)).strftime('%Y/%m/%d')
+last_date_data = datetime.date.today() -datetime.timedelta(1)
+last_date = last_date_data.strftime('%Y/%m/%d')
 bef_last_date = (datetime.date.today() -datetime.timedelta(2)).strftime('%Y/%m/%d')
 
 
 # ==== 関数get_value() ====
 # ※ get_value(サービス名)で昨日までのサービス利用料金を取得する
 #   サービス名が「ALL」の場合は全体の金額を取得する
-def get_value(service_name):
+def get_value(service_name,check_day):
     if service_name == 'ALL':
         get_demesion = [{'Name': 'Currency', 'Value': 'USD'}]
     else:
@@ -48,12 +48,12 @@ def get_value(service_name):
            Namespace='AWS/Billing',
            MetricName='EstimatedCharges',
            Period=86400,
-           StartTime=last_date + " 00:00:00",
-           EndTime=last_date + " 23:00:00",
+           StartTime=check_day + " 00:00:00",
+           EndTime=check_day + " 23:59:59",
            Statistics=['Maximum'],
            Dimensions=get_demesion
            )
-    for info in data['Datapoints']: 
+    for info in data['Datapoints']:
         return info['Maximum']
 
 
@@ -61,6 +61,7 @@ def get_value(service_name):
 # もうちょっと良い書き方が無いものか…
 service_list = []
 service_value = []
+bef_service_value = []
 json_value = cloudwatch_conn.list_metrics()
 for attr1 in json_value.get('Metrics'):
     for attr2 in attr1.get('Dimensions'):
@@ -71,22 +72,32 @@ service_list.sort()
 
 # ==== サービス別の金額を取得する ====
 for service in service_list:
-    service_value.append(get_value(service))
+    service_value.append(get_value(service,last_date))
 
+for service in service_list:
+    bef_service_value.append(get_value(service,bef_last_date))
 
 # ==== トータルの金額を取得する ====
-total_value = get_value('ALL')
-
+last_total_value = get_value('ALL',last_date)
+bef_last_total_value = get_value('ALL',bef_last_date)
+if last_date_data.day == 1:
+    total_before_ratio = last_total_value
+else:
+    total_before_ratio = last_total_value - bef_last_total_value
 
 # ==== SlackへPostする ====
-SLACK_TEXT='昨日までのAWSの利用料金は$' + str(total_value) + 'になります'
+SLACK_TEXT='昨日までのAWSの利用料金は *$' + str(last_total_value) + '* (前日比 + *$' + str(total_before_ratio) + '* )' + 'になります'
 
 slack=slackweb.Slack(url=SLACK_WEBHOOK)
 attachments=[]
 attachment={'pretext': '各サービス別の利用料金','fields': []}
 
 for var in range(0, len(service_list)):
-    item={'title': service_list[var] ,'value': '$' + str(service_value[var]) ,'short': "true"}
+    if last_date_data.day == 1:
+      before_ratio=service_value[var]
+    else:
+      before_ratio=service_value[var] - bef_service_value[var]
+    item={'title': service_list[var] ,'value': ' $' + str(service_value[var]) + ' (前日比 +$' + str(before_ratio) + ') ' ,'short': "true"}
     attachment['fields'].append(item)
 
 attachments.append(attachment)
